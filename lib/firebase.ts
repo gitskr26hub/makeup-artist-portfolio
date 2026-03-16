6e3e93b4c3150424c06a271394351f7c5c6eb46e
gitskr26hub <kumarsugam26@gmail.com>
2026-03-16 20:47:06 +0530

    add google login

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY!,
@@ -8,8 +9,10 @@ const firebaseConfig = {
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
 
 
-export default db;
+
 
--- repositories/admin.repository.ts
+++ repositories/admin.repository.ts
@@ -1,4 +1,5 @@
-import db from "@/lib/firebase";
+
+import { db } from "@/lib/firebase";
 import {
     doc,
     getDoc,
